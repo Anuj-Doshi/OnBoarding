c544d3688f84dfd898ad699aadc84583ec413511
Anuj Doshi <42434014+Anuj-Doshi@users.noreply.github.com>
2021-01-27 16:59:53 +0530

    Add files via upload

---
+++ Demo/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Demo
+//
+//  Created by Aipxperts on 27/01/21.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Demo/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Aipxperts on 27/01/21.
+//
+
+import UIKit
+import SwiftyOnboard
+
+class ViewController: UIViewController {
+    var swiftyOnboard: SwiftyOnboard!
+    let colors:[UIColor] = [#colorLiteral(red: 0.9980840087, green: 0.3723873496, blue: 0.4952875376, alpha: 1),#colorLiteral(red: 0.2666860223, green: 0.5116362572, blue: 1, alpha: 1),#colorLiteral(red: 0.1019607857, green: 0.2784313858, blue: 0.400000006, alpha: 1)]
+    var titleArray: [String] = ["Let's start", "Welcome", "Finish"]
+    var subTitleArray: [String] = ["s1","s2","s3"]
+    var gradiant: CAGradientLayer = {
+            //Gradiant for the background view
+            let blue = UIColor(red: 69/255, green: 127/255, blue: 202/255, alpha: 1.0).cgColor
+            let purple = UIColor(red: 166/255, green: 172/255, blue: 236/255, alpha: 1.0).cgColor
+            let gradiant = CAGradientLayer()
+            gradiant.colors = [purple, blue]
+            gradiant.startPoint = CGPoint(x: 0.5, y: 0.18)
+            return gradiant
+        }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        gradient()
+        swiftyOnboard = SwiftyOnboard(frame: view.frame, style: .light)
+        view.addSubview(swiftyOnboard)
+        swiftyOnboard.dataSource = self
+        swiftyOnboard.delegate = self
+        
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+        
+    func gradient() {
+        //Add the gradiant to the view:
+        self.gradiant.frame = view.bounds
+        view.layer.addSublayer(gradiant)
+    }
+        
+    @objc func handleSkip() {
+        swiftyOnboard?.goToPage(index: 2, animated: true)
+        
+    }
+        
+    @objc func handleContinue(sender: UIButton) {
+        let index = sender.tag
+        swiftyOnboard?.goToPage(index: index + 1, animated: true)
+    }
+}
+extension ViewController: SwiftyOnboardDelegate, SwiftyOnboardDataSource {
+    
+    func swiftyOnboardNumberOfPages(_ swiftyOnboard: SwiftyOnboard) -> Int {
+        return 3
+    }
+    
+    func swiftyOnboardBackgroundColorFor(_ swiftyOnboard: SwiftyOnboard, atIndex index: Int) -> UIColor? {
+        //Return the background color for the page at index:
+        return colors[index]
+    }
+    
+    func swiftyOnboardPageForIndex(_ swiftyOnboard: SwiftyOnboard, index: Int) -> SwiftyOnboardPage? {
+        let view = SwiftyOnboardPage()
+        view.imageView.image = UIImage(named: "onboard\(index)")
+        view.title.font = UIFont(name: "Lato-Heavy", size: 22)
+        view.subTitle.font = UIFont(name: "Lato-Regular", size: 16)
+        view.title.text = titleArray[index]
+        view.subTitle.text = subTitleArray[index]
+        return view
+    }
+    
+    func swiftyOnboardViewForOverlay(_ swiftyOnboard: SwiftyOnboard) -> SwiftyOnboardOverlay? {
+        let overlay = SwiftyOnboardOverlay()
+        
+        overlay.skipButton.addTarget(self, action: #selector(handleSkip), for: .touchUpInside)
+        overlay.continueButton.addTarget(self, action: #selector(handleContinue), for: .touchUpInside)
+        overlay.continueButton.titleLabel?.font = UIFont(name: "Lato-Bold", size: 16)
+        overlay.continueButton.setTitleColor(UIColor.white, for: .normal)
+        overlay.skipButton.setTitleColor(UIColor.white, for: .normal)
+        overlay.skipButton.titleLabel?.font = UIFont(name: "Lato-Heavy", size: 16)
+        return overlay
+    }
+    
+    func swiftyOnboardOverlayForPosition(_ swiftyOnboard: SwiftyOnboard, overlay: SwiftyOnboardOverlay, for position: Double) {
+        let currentPage = round(position)
+        overlay.pageControl.currentPage = Int(currentPage)
+        print(Int(currentPage))
+        overlay.continueButton.tag = Int(position)
+        
+        if currentPage == 0.0 || currentPage == 1.0 {
+            overlay.continueButton.setTitle("Continue", for: .normal)
+            overlay.skipButton.setTitle("Skip", for: .normal)
+            overlay.skipButton.isHidden = false
+        } else {
+            overlay.continueButton.setTitle("Get Started!", for: .normal)
+            overlay.skipButton.isHidden = true
+        }
+    }
+}
